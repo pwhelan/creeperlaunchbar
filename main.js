8506d3acd27d52155dd0d2286d1dcca4eb6fa1e0
Phillip Whelan <pwhelan@mixxx.org>
2014-08-10 13:34:42 -0700

    De-register global shortcut when closing down the launcher.

--- main.js
+++ main.js
@@ -206,37 +206,12 @@ ipc
 	});
 
 
-/*
-// Unregister a shortcut.
-globalShortcut.unregister('ctrl+space');
-
-// Unregister all shortcuts.
-globalShortcut.unregisterAll();
-*/
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
+	
+	globalShortcut.unregister('ctrl+space');
+	globalShortcut.unregisterAll();
+	
 	if (process.platform != 'darwin')
 		app.quit();
 });
-
-/*
-// This method will be called when atom-shell has done everything
-// initialization and ready for creating browser windows.
-app.on('ready', function() {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({width: 800, height: 600});
-	
-	// and load the index.html of the app.
-	mainWindow.loadUrl('file://' + __dirname + '/index.html');
-	
-	// Emitted when the window is closed.
-	mainWindow.on('closed', function() {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		mainWindow = null;
-	});
-});
-
-*/
