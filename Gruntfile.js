a4deccc30c1233347666b15b865cb0ee51104442
Phillip Whelan <pwhelan@mixxx.org>
2014-08-16 17:08:54 -0700

    Begin creating build tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,65 @@
 module.exports = function(grunt) {
 	
+	var AppFiles = [
+		'main.js',
+		'index.html',
+		'lib/*.js',
+		'lib/**/*.js'
+	];
+	
 	grunt.initConfig({
 		'download-atom-shell': {
 			version: '0.15.4',
-			outputDir: 'binaries'
+			outputDir: 'binaries',
+			downloadDir: 'cache/atom-shell-download/'
 		},
 		watch: {
 			'atom-shell': {
-				files: [
-					'main.js',
-					'index.html',
-					'lib/*.js',
-					'lib/**/*.js'
-				],
+				files: AppFiles,
 				tasks: ['atom-shell:restart']
+			}
+		},
+		copy: {
+			appbundle: {
+				files: [{
+					expand: true,
+					cwd: 'binaries/Atom.app/',
+					src: [
+						'Contents/Info.plist',
+						'Contents/PkgInfo',
+						'Contents/MacOS/Atom',
+						'Contents/Resources',
+						'Contents/Frameworks/Atom Framework.framework/Atom Framework',
+						'Contents/Frameworks/Atom Framework.framework/Libraries/*.so',
+						'Contents/Frameworks/Atom Framework.framework/Libraries/*.dylib',
+						'Contents/Frameworks/Atom Framework.framework/Resources/**',
+						'Contents/Frameworks/Atom Framework.framework/Versions/**',
+						'Contents/Frameworks/Atom Helper EH.app/**',
+						'Contents/Frameworks/Atom Helper NP.app/**',
+						'Contents/Frameworks/Atom Helper.app/**',
+						'Contents/Frameworks/Mantle.framework/**',
+						'Contents/Frameworks/ReactiveCocoa.framework/**',
+						'Contents/Frameworks/Squirrel.framework/**',
+					],
+					dest: 'build/Creeper.app/',
+					process: function(contents, srcpath, dstpath) {
+						var fs = require('fs');
+						var stat = fs.lstat(srcpath);
+						
+						if (stat.isSymbolicLink()) {
+							return false;
+						}
+						
+						return true;
+					}
+				}]
 			}
 		}
 	});
 	
 	grunt.loadNpmTasks('grunt-download-atom-shell');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	
 	grunt.registerTask('atom-shell:start', 'Run Launchdd', function() {
 		
@@ -87,4 +127,52 @@ module.exports = function(grunt) {
 	});
 	
 	grunt.registerTask('atom-shell:restart', ['atom-shell:stop', 'atom-shell:start']);
+	
+	grunt.registerTask('build', 'Build Application', function() {
+		
+		if (!grunt.file.exists('./build/')) {
+			grunt.file.mkdir('./build/');
+		}
+		
+		var tasks = ['download-atom-shell'];
+		
+		if (process.platform == 'darwin')
+		{
+			tasks.push('copy:appbundle');
+			tasks.push('build:appbundle');
+		}
+		
+		grunt.task.run(tasks);
+	});
+	
+	grunt.registerTask('build:appbundle', 'Build Application', function() {
+		
+		if (!grunt.file.exists('build/Creeper.app'))
+		{
+			
+		}
+		
+		var baseAppDir = './build/Creeper.app/Contents/Resources/app';
+		var path = require('path');
+		
+		
+		if (!grunt.file.exists(baseAppDir)) {
+			grunt.file.mkdir(baseAppDir);
+		}
+		
+		var files = grunt.file.expand(AppFiles);
+		for (var n in files)
+		{
+			if (!grunt.file.exists(baseAppDir + '/' + path.dirname(files[n])))
+			{
+				grunt.file.mkdir(baseAppDir + '/' + path.dirname(files[n]));
+			}
+			
+			grunt.file.copy(
+				files[n],
+				baseAppDir + '/' + files[n]
+			);
+		}
+	});
+	
 };
