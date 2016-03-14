c62183eeba09f07915de811e806e1ff500c02227
Phillip Whelan <pwhelan@exis.cl>
2016-03-13 23:34:44 -0300

    Add some more test cases.

--- test/calculator.js
+++ test/calculator.js
@@ -6,3 +6,5 @@ assert.equal(calc.calculate('1 + 2 + 3'), 6);
 assert.equal(calc.calculate('4 + 2 * 3 + 1'), 11);
 assert.equal(calc.calculate('4 + 2 * (3 + 1)'), 12);
 assert.equal(calc.calculate('(4 + 2) * (3 + 1)'), 24);
+assert.equal(calc.calculate('(4 * 2 + 1) * (3 + 1)'), 36);
+assert.equal(calc.calculate('(4 * (2 + 1)) * (3 + 1)'), 48);
